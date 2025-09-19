fdc2f6bf0d968c0777bce58e7c2d032b1423c7eb
ebarbozag-cpu <ebarbozag@ucenfotec.ac.cr>
2025-09-18 19:44:37 -0600

    segunda parte

--- Semana 3/Clase 1/Ejercicio 1.py
+++ Semana 3/Clase 1/Ejercicio 1.py
@@ -2,13 +2,17 @@
 
 nombre_producto = ""
 precio_producto = 0.0
-iva = 13
+precio_final = 0.0
 
 #Solicitar entradas
 
-
+nombre_producto = str(input("Ingrese el nombre del producto:"))
+precio_producto = float(input("Ingrese el precio del producto:"))
 
 #Realizar operación
 
+precio_final = precio_producto + ((13 / 100) * precio_producto)
 
 #Salidas
+
+print("El precio del producto ",nombre_producto, "es de",precio_final)
--- Semana 3/Clase 1/Ejercicio 6.py
+++ Semana 3/Clase 1/Ejercicio 6.py
@@ -1,16 +1,20 @@
 #Definir Variables
 
+porcentaje_impuesto = 0.0
+salario_bruto = 0.0
+salario_neto = 0.0
 
 
 #Solicitar entradas
 
-
-
+salario_bruto = float(input("Ingrese el salario bruto:"))
+porcentaje_impuesto = float(input("Ingrese el impuesto a aplicar:"))
 
 #Procesos
 
-
+salario_neto = salario_bruto - ((porcentaje_impuesto / 100) * salario_bruto)
 
 
 #Salidas
 
+print("El salario neto a pagar es",salario_neto)
