a5be113defe6c35b3bbee29b6c168399c1b2a756
ebarbozag-cpu <ebarbozag@ucenfotec.ac.cr>
2025-09-30 19:27:34 -0600

    2

--- Semana 4/Clase 2/Ejercicio 10.py
+++ Semana 4/Clase 2/Ejercicio 10.py
@@ -1,5 +1,17 @@
 #Definir Variables
 
+numero = 0
+
 #Solicitar entradas
 
+numero = int(input("Digite el número:"))
+
 #Procesos/salidas
+
+if numero == 0:
+    print("Su numero es un 0")
+elif numero < 0:
+    print("El número es negativo")
+else:
+    numero > 0
+    print("El número es positivo")
